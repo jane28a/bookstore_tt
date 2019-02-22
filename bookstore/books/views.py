95933a6751ec4fb8b358e7d3f18d77512f43db5a
Jane Karaim <janekaraim@gmail.com>
2019-02-22 16:23:55 +0200

    view for books list added

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotAllowed
 
-# Create your views here.
+from .models import Book
+
+
+def index(request):
+
+    '''Index page of site with the list of all books'''
+
+    #Only GET requests are accepted by this view
+    if request.method == 'GET':
+        books = Book.objects.all()
+        return render(request, 'books/index.html', {'books': books})
+    else:
+        #For other request methods return response with status 405 and
+        #list with only allowed method
+        return HttpResponseNotAllowed(['GET'])
