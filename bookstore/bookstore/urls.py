706c3cfacaf0070ea7e170027bd223b765a8edfd
Jane Karaim <janekaraim@gmail.com>
2019-02-23 13:48:17 +0200

    login machinery added

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', lambda request: redirect('books:index')),
     path('books/', include('books.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
